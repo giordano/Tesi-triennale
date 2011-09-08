75e37ce4e52c0f28c055a4662540dd931ce5852b
Mosè Giordano <giordano.mose@libero.it>
2011-09-08 17:27:05 +0200

    nuova correzione al programma per la simulazione dell'eclissi
    Le funzioni per la simulazione dell'eclissi erano corrette, continuavo
    a ottenere risultati sbagliati perché avevo inserito valori iniziali
    (in particolare il semiasse maggiore) non adatti (risultava: a < r1+r2).
    Per evitare altre sorprese del genere in futuro ho aggiunto nel programma
    un controllo per la coerenza dei dati (non è un controllo completo ma
    sicuramente meglio di niente).
    
    Ho rinominato qualche file e fatto qualche altro cambiamente che non mi
    va di commentare :-P

---
+++ programmi/eclissi.c
@@ -0,0 +1,113 @@
+/* Nome del file sorgente: pianeti.c
+ * Scopo: simulazione di un'eclissi di una stella dietro a un pianeta.
+ * Dati di input: raggi e masse dei due corpi, semiasse maggiore ed
+ * eccentricità delle orbite, luminosità della stella, angoli di
+ * periapside e di inclinazione. Unità di misura usate: CGS.
+ * Output: un file di testo con i risultati della simulazione. Il file
+ * è così organizzato: è suddiviso in 15 colonne, nella prima c'è la
+ * fase (fase=(t-t_iniziale)/periodo); nelle 3 colonne successive ci
+ * sono le coordinate x, y, z iniziali dell particella fittizia; nelle
+ * 3 colonne successive ci sono le coordinate x'', y'' e z'' della
+ * particella fittizia nel piano del cielo; nelle 3 colonne successive
+ * ci sono le coordinate x1'', y1'' e z1'' della stella (di massa m1)
+ * nel piano del cielo; nelle 3 colonne successive ci sono le coordinate
+ * x2'', y2'' e z2'' del pianeta (di massa m2) nel piano del cielo; nella
+ * colonna 14 c'è la distanza fra stella e pianeta proiettata nel piano
+ * del cielo e normalizzata alla somma dei raggi dei due corpi; nella colonna
+ * 15 c'è il flusso luminoso osservato a Terra e normalizzato a 1. Il file
+ * può essere letto da un software per la realizzazione di grafici.
+ * Autore: Mosè Giordano
+ * Data: 06/09/2011
+ */
+
+#include <stdio.h>
+#include <gsl/gsl_const_cgsm.h> /* header della GSL contenente il valore di
+				 * alcune costanti fisiche in unità CGSM */
+#include "libreria.h"
+
+/* Numero di punti in cui effettuare la simulazione */
+#define PUNTI 5000
+
+int main(){
+  /* Definizione delle variabili */
+  double r1, r2; /* r1 = raggio stella, r2 = raggio pianeta */
+  double m1, m2, mu; /* masse dei due corpi e massa ridotta */
+  double a; /* semiasse maggiore dell'orbita */
+  double e; /* eccentricità dell'orbita */
+  double periodo; /* periodo dell'orbita */
+  double t; /* istante di tempo */
+  double tmin, tmax; /* istanti di tempo minimo e massimo in
+		      * cui effettuare i calcoli */
+  double lum; /* luminosità intrinseca della stella */
+  double omega; /* velocità angolare media */
+  double psi, theta, r; /* anomalie eccentrica e vera e distanza dal fuoco */
+  /* vettori di posizione della particella fittizia rispettivamente nel piano
+   * solidale al sistema binario e nel piano del cielo */
+  double ppf[3], ppfpc[3];
+  /* vettori di posizione, nel piano del cielo, rispettivamente
+   * della particella di massa m1 e di massa m2 */
+  double p1pc[3], p2pc[3];
+  double phi, i; /* angoli delle rotazioni */
+  double d; /* distanza proiettata nel piano del cielo fra stella e pianeta */
+  double dA; /* area della stella coperta dal pianeta */
+  double F; /* flusso luminoso */
+  FILE *eclissi; /* puntatore a file */
+
+  /* Inizializzazione delle variabili */
+  r1=1e12;
+  r2=5e11;
+  m1=1.*GSL_CONST_CGSM_SOLAR_MASS; /* 1 massa solare */
+  m2=0.05*GSL_CONST_CGSM_SOLAR_MASS;     /* 0.1 masse solari */
+  mu=m1*m2/(m1+m2);
+  a=1e13;
+  e=0.8;
+  /* calcolo il periodo orbitale usando la terza legge di Keplero */
+  periodo=sqrt(4*M_PI*M_PI*a*a*a/(GSL_CONST_CGSM_GRAVITATIONAL_CONSTANT*(m1+m2)));
+  tmin=0; /* al periapside t=0 */
+  tmax=tmin+2*periodo;
+  lum=1;
+  omega=2*M_PI/periodo;
+  phi=30./180.*M_PI;
+  i=85./180.*M_PI;
+  /* la coordinata z della particella fittizia è sempre nulla */
+  ppf[2]=0;
+
+  /* Effettuiamo alcuni controlli sui dati iniziali */
+  if(a <= r1+r2)
+    {
+      fprintf(stderr,"Errore: il semiasse maggiore è più piccolo della somma dei raggi dei due corpi.\n");
+      return 1;
+    }
+  if(r1<r2)
+    {
+      fprintf(stderr,"Errore: il raggio della stella è minore del raggio del pianeta. Si consiglia di invertirli.\n");
+      return 1;
+    }
+
+  /* apro il file su cui scrivere i dati */
+  eclissi=fopen("eclissi.dat","w");
+  /* Eseguo la simulazione per ognuno dei PUNTI istanti dell'intervallo
+   * di tempo [tmin,tmax] */
+  for(t=tmin;t<=tmax;t+=(tmax-tmin)/PUNTI)
+    {
+      psi=psi_bessel(omega*t,e); /* calolo l'anomalia eccentrica */
+      r=r_bessel(omega*t,a,e);   /* calcolo la distanza dal fuoco */
+      theta=anomvera(e,psi);     /* calcolo l'anomalia vera */
+      ppf[0]=r*cos(theta); /* coordinata x della particella fittizia */
+      ppf[1]=r*sin(theta); /* coordinata y della particella fittizia */
+      /* calcolo le coordinate della particella fittizia e dei due corpi nel
+       * piano del cielo */
+      pianodelcielo(ppf, phi, i, ppfpc);
+      vettore_scalare(3,ppfpc,p1pc,-mu/m1);
+      vettore_scalare(3,ppfpc,p2pc,mu/m2);
+      d=hypot(ppfpc[1],ppfpc[2]); /* calcolo la distanza proiettata */
+      dA=area_coperta(r1, r2, d, p1pc[0], p2pc[0]);
+      F=flusso(4,lum,r1,dA);
+      /* scrivo su file i risultati */
+      fprintf(eclissi,"%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",
+	      (t-tmin)/periodo,ppf[0],ppf[1],ppf[2],ppfpc[0],ppfpc[1],ppfpc[2],
+	      p1pc[0],p1pc[1],p1pc[2],p2pc[0],p2pc[1],p2pc[2],d/(r1+r2),F);
+    }
+  fclose(eclissi);
+  return 0;
+}
--- programmi/keplero.c
+++ programmi/keplero.c
@@ -4,13 +4,13 @@
  * e con quello dei coefficienti di Bessel.
  * Dati di input: semiasse maggiore, eccentricità e periodo
  * delle orbite.
- * Output: due file con i risultati ottenuti con i due metodi
- * differenti. I file sono così organizzati: sono suddiviso in
- * dieci colonne, nella prima c'è il tempo t, nelle nove
+ * Output: due file di testo con i risultati ottenuti con i due
+ * metodi differenti. I file sono così organizzati: sono suddiviso
+ * in dieci colonne, nella prima c'è il tempo t, nelle nove
  * successive ci sono i valori delle anomalie eccentriche,
  * distanze dal fuoco e anomalie vere al tempo t per i tre
  * differenti valori dell'eccentricità. I file possono essere
- * letti da un programma per la realizzazione di grafici.
+ * letti da un software per la realizzazione di grafici.
  * Autore: Mosè Giordano
  * Data: 02/09/2011
  */
@@ -28,19 +28,19 @@ int main(){
   double a; /* semiasse maggiore dell'orbita */
   double e[N]={0,0.5,0.8}; /* eccentricità delle orbite */
   double periodo;   /* periodo dell'orbita */
-  double t; /* tempo in cui calcolare l'anomalia eccentrica */
+  double t; /* istante di tempo tempo */
   double tmin, tmax; /* istanti di tempo minimo e massimo in
 		      * cui calcolare l'anomalia eccentrica */
   double omega; /* velocità angolare media */
   double psi; /* anomalia eccentrica */
-  int i; /* contatore da usare nel ciclo for */
+  int i; /* contatore */
   FILE *newton, *bessel; /* puntatori a file */
 
   /* Inizializzazione delle variabili */
   a=2.5; /* 2.5·10^6 Km */
   periodo=10; /* 10 ore */
-  tmin=0; /* Abbiamo supposto che al periapside t=0 */
-  tmax=periodo;
+  tmin=0; /* al periapside t=0 */
+  tmax=tmin+periodo;
   omega=2*M_PI/periodo;
 
   /* Apro i file su cui scrivere i risultati */
